86a5c71c669dbab60cd58a8dbf58c1b1bbac5e88
scuxiatian <scuxiatian@foxmail.com>
2020-02-17 19:44:27 +0800

    update REDEME

--- README.md
+++ README.md
@@ -1,24 +1,26 @@
-# my-vue-template
+English | [简体中文](./README.zh-CN.md)
 
-## Project setup
-```
-npm install
-```
+## Introduction
 
-### Compiles and hot-reloads for development
-```
-npm run serve
-```
+[my-vue-template](https://github.com/scuxiatian/my-vue-template) is a lightweight production-ready front-end solution for admin interfaces. It based on [vue](https://github.com/vuejs/vue) and use the UI Toolkit [element-ui](https://github.com/ElemeFE/element).
 
-### Compiles and minifies for production
-```
-npm run build
-```
+## Preparation
 
-### Lints and fixes files
-```
-npm run lint
-```
+You need to install [node](https://nodejs.org/) and [git](https://git-scm.com/) locally. The project is based on [ES2015+](https://es6.ruanyifeng.com/), [vue](https://cn.vuejs.org/index.html), [vuex](https://vuex.vuejs.org/zh-cn/), [vue-router](https://router.vuejs.org/zh-cn/), [vue-cli](https://github.com/vuejs/vue-cli) , [axios](https://github.com/axios/axios) and [element-ui](https://github.com/ElemeFE/element), all request data is simulated using [Mock.js](https://github.com/nuysoft/Mock).
+Understanding and learning this knowledge in advance will greatly help the use of this project.
 
-### Customize configuration
-See [Configuration Reference](https://cli.vuejs.org/config/).
+## Installation steps
+
+    git clone https://github.com/scuxiatian/my-vue-template.git		// Clone templates
+    cd my-vue-template											// Enter template directory
+    npm install													// Installation dependency
+
+## Local development
+
+    // Open server and access http://localhost:8080 in browser
+    npm run serve
+
+## Constructing production
+
+    // Constructing project
+    npm run build
--- src/router/index.js
+++ src/router/index.js
@@ -6,6 +6,60 @@ import store from '@/store'
 
 Vue.use(Router)
 
+// 侧边栏中的路由
+const sidebarRouter = [
+  {
+    path: 'dashboard',
+    component: () => import('@/views/dashboard'),
+    meta: { title: '系统首页' }
+  },
+  {
+    path: 'charts',
+    component: () => import('@/views/baseCharts'),
+    meta: { title: 'Echarts图表' }
+  },
+  {
+    path: 'icon',
+    component: () => import('@/views/icon'),
+    meta: { title: '图标' }
+  },
+  {
+    path: 'markdown',
+    component: () => import('@/views/markdown'),
+    meta: { title: 'Markdown编辑器' }
+  },
+  {
+    path: 'permission',
+    component: () => import('@/views/permission'),
+    meta: { title: '权限测试', admin: true }
+  },
+  {
+    path: 'excelDownload',
+    component: () => import('@/views/excel/download'),
+    meta: { title: 'Excel导出' }
+  },
+  {
+    path: 'excelUpload',
+    component: () => import('@/views/excel/upload'),
+    meta: { title: 'Excel导入' }
+  },
+  {
+    path: '404',
+    component: () => import('@/views/errors/404.vue'),
+    meta: { title: '404' }
+  },
+  {
+    path: '401',
+    component: () => import('@/views/errors/401.vue'),
+    meta: { title: '401' }
+  },
+  {
+    path: 'donate',
+    component: () => import('@/views/donate'),
+    meta: { title: '支持作者' }
+  }
+]
+
 const router = new Router({
   routes: [
     {
@@ -16,58 +70,7 @@ const router = new Router({
       path: '/',
       component: Layout,
       meta: { title: '自述文件' },
-      children: [
-        {
-          path: 'dashboard',
-          component: () => import('@/views/dashboard'),
-          meta: { title: '系统首页' }
-        },
-        {
-          path: 'charts',
-          component: () => import('@/views/baseCharts'),
-          meta: { title: 'Echarts图表' }
-        },
-        {
-          path: 'icon',
-          component: () => import('@/views/icon'),
-          meta: { title: '图标' }
-        },
-        {
-          path: 'markdown',
-          component: () => import('@/views/markdown'),
-          meta: { title: 'Markdown编辑器' }
-        },
-        {
-          path: 'permission',
-          component: () => import('@/views/permission'),
-          meta: { title: '权限测试', admin: true }
-        },
-        {
-          path: 'excelDownload',
-          component: () => import('@/views/excel/download'),
-          meta: { title: 'Excel导出' }
-        },
-        {
-          path: 'excelUpload',
-          component: () => import('@/views/excel/upload'),
-          meta: { title: 'Excel导入' }
-        },
-        {
-          path: '404',
-          component: () => import('@/views/errors/404.vue'),
-          meta: { title: '404' }
-        },
-        {
-          path: '401',
-          component: () => import('@/views/errors/401.vue'),
-          meta: { title: '401' }
-        },
-        {
-          path: 'donate',
-          component: () => import('@/views/donate'),
-          meta: { title: '支持作者' }
-        }
-      ]
+      children: sidebarRouter
     },
     {
       path: '/login',
